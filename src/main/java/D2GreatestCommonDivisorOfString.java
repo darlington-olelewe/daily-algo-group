f8d3fc71086ddb3e104ab86417fa5af68d512a3e
Darlington Olelewe <o.darlington@outlook.com>
2024-07-12 21:35:15 +0100

    day 1 solution to leetcode 1768

---
+++ src/main/java/D2GreatestCommonDivisorOfString.java
@@ -0,0 +1,32 @@
+public class D2GreatestCommonDivisorOfString {
+    /**
+     * Code         1071
+     * Difficulty   easy
+     * Question     For two strings s and t, we say "t divides s" if and only
+     *              if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+     *              Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+     * link = "https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75"
+     * todo         Drop copy and paste the content of method/function into the solution section on leetcode
+     */
+
+    public String gcdOfStrings(String str1, String str2) {
+
+        int left = str1.length();
+        int right = str2.length();
+
+        if(right > left){
+            return gcdOfStrings(str2,str1);
+        }
+
+        if(str1.equals(str2)){
+            return str1;
+        }
+
+        if(str1.startsWith(str2)){
+            return gcdOfStrings(str2, str1.substring(str2.length()));
+        }
+
+        return "";
+
+    }
+}
---
+++ src/test/java/D2GreatestCommonDivisorOfStringTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class D2GreatestCommonDivisorOfStringTest {
+
+    private D2GreatestCommonDivisorOfString gcd = new D2GreatestCommonDivisorOfString();
+
+    @Test
+    public void gcdOfStrings_toReturn_ABC() {
+
+        String str1 = "ABCABC";
+        String str2 = "ABC";
+
+        String expected = gcd.gcdOfStrings(str1,str2);
+        String actual = "ABC";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gcdOfStrings_toReturn_AB() {
+
+        String str1 = "ABABAB";
+        String str2 = "ABAB";
+
+        String expected = gcd.gcdOfStrings(str1,str2);
+        String actual = "AB";
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void gcdOfStrings_toReturn_empty() {
+
+        String str1 = "LEET";
+        String str2 = "CODE";
+
+        String expected = gcd.gcdOfStrings(str1,str2);
+        String actual = "";
+
+        assertEquals(expected, actual);
+    }
+}
