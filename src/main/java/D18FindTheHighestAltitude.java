4174874bccdfc12b45bc2482f1d63ba60a7e3a07
Darlington Olelewe <o.darlington@outlook.com>
2024-07-28 21:02:31 +0100

    day 18 solution to leetcode 1732

--- src/main/java/D18FindTheHighestAltitude.java
+++ src/main/java/D18FindTheHighestAltitude.java
@@ -1,15 +1,16 @@
-public class D17LongestSubArrayOf1sAfterDeletingOneElement {
-    /**
-     * Code         1493
-     * Difficulty   medium
-     * Question     Given a binary array nums, you should delete one element from it.
-     *              Return the size of the longest non-empty subarray containing only 1's in the resulting array.
-     *              Return 0 if there is no such subarray.
-     *
-     * link = "https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/description/?envType=study-plan-v2&envId=leetcode-75"
-     */
+public class D18FindTheHighestAltitude {
 
     public int longestSubarray(int[] nums) {
+        /**
+         * Code         1732
+         * Difficulty   easy
+         * Question     There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+         *              The biker starts his trip on point 0 with altitude equal 0.
+         *              You are given an integer array gain of length n where gain[i] is the net gain in altitude between
+         *              points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+         *
+         * link = "https://leetcode.com/problems/find-the-highest-altitude/?envType=study-plan-v2&envId=leetcode-75"
+         */
         int lastIndex = -1;
         int maxOnes = 0;
         int count = 0;
