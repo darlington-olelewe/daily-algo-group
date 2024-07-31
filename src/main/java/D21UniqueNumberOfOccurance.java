1ea125a3f5d6a4912a53e58ae8ebd13ab191bd78
Darlington Olelewe <o.darlington@outlook.com>
2024-07-31 23:16:01 +0100

    day 21 solution to leetcode 1207

---
+++ src/main/java/D21UniqueNumberOfOccurance.java
@@ -0,0 +1,11 @@
+public class D21UniqueNumberOfOccurance {
+    /**
+     * Code         1207
+     * Difficulty   easy
+     * Question     Given an array of integers arr, return true if the number of occurrences of
+     *              each value in the array is unique or false otherwise.
+     *
+     * link = "Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise."
+     */
+}
+
